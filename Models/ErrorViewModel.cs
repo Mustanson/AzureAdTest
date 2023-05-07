bdca0e4fceb917251ac51563de8fa4e5f35a11d1
skhan777 <78987980+skhan777@users.noreply.github.com>
2023-05-06 22:42:10 -0500

    azure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAdTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
